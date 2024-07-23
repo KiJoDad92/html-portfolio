744d70af46af83c57050803f5affde2dc8decd70
unknown <kirkjholt@gmail.com>
2024-07-22 22:29:29 -0600

    added starter files

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+import fs from "fs";
+
+inquirer
+prompt([
+{
+    type: 
+    message: 
+    name: 
+  },
+  {
+    type: 
+    message: 
+    name: 
+  },
+  {
+    type: 
+    message: 
+    name: 
+  },
+])
